a3eaeca904444ab755b35f62e0ea2b3e31fac0aa
Brandon Reyes <brandon.reyesmi@gmail.com>
2020-10-23 22:31:26 -0500

    Dark mode implemented and cards section

--- src/SwapiApp.js
+++ src/SwapiApp.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card from './components/Card'
 import Header from './components/Header'
 
 const SwapiApp = () => {
+    const [title, setTitle] = useState('Select a movie')
     return (
-        <div>
+        <>
             <Header/>
-            
-        </div>
+            <div className="cards-section shadow-xl">
+                <div className="cards-container">
+                    <h2>{title}</h2>
+                    <div className="grid grid-cols-3 gap-4 mt-10">
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                    </div>
+
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,6 @@
 import React from 'react'
 
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-
 const Header = () => {
-
-    const [state, setState] = React.useState({
-        checkedA: true,
-        checkedB: true,
-      });
-    
-      const handleChange = (event) => {
-        setState({ ...state, [event.target.name]: event.target.checked });
-      };
-
 
     return (
         <div className="header">
